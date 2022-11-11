5ccab8432f6cc048bec1d50f07470c531ffebf43
Hogale <262164@NTTDATA.COM>
2022-11-11 11:09:37 +0530

    login for admin and user added

--- hpifront/src/app/app-routing.module.ts
+++ hpifront/src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboard.component';
+import { AdminGuard } from './services/admin.guard';
+import { NormalGuard } from './services/normal.guard';
 
 const routes: Routes = [
   {
@@ -25,11 +27,13 @@ const routes: Routes = [
     path:'admin',
     component:DashboardComponent,
     pathMatch:'full',
+    canActivate:[AdminGuard],
   },
   {
     path:'user-dashboard',
     component:UserDashboardComponent,
     pathMatch:'full',
+    canActivate:[NormalGuard],
   }
 ];
 
